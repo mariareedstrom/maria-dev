9420d07d1e0982372d66801bc8c6e007167e9a79
Maria Reedstrom <maria.reedstrom@gmail.com>
2023-04-04 14:22:42 -0500

    added blog posts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
-import { FaGithub, FaLaptop, FaLinkedinIn, FaMedium } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaMedium } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-600 sticky-top-0 drop-shadow-xl z-10  ">
+    <nav className="bg-slate-600 sticky-top-0 drop-shadow-xl z-10">
       <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
         <h3 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
           <Link
